87b8e3831f659138b6e9dea1af5324c0009f4589
denis ermurachi <denisermurachi06@gmail.com>
2025-03-05 18:10:14 +0100

    Client Configuration

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d99635e464ab66d33fb054e4fbf520441da12d5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38d7ee8aa81fa5878d986e7d95417f4141dfdaf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
